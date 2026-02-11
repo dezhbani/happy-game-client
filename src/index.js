3a2b36499c20852f55a118c99c305cff72ffa525
dezhbani <93706202+dezhbani@users.noreply.github.com>
2026-02-11 19:18:26 +0330

    Update api domain

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import UserContext from './contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = 'https://api-happygame.liara.run';
+axios.defaults.baseURL = 'https://happy-game-server.vercel.app';
 // for use local API 
 // axios.defaults.baseURL = 'http://localhost:2000'; 
 root.render(
