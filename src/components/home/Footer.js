b002f73b54ad47f922a244e65260503e2c5d4738
matin dezhbani <dezhbanimatin@gmail.com>
2026-05-06 14:39:09 +0330

    refactor: add footer in background component & get props to show and hidden footer in page

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -30,73 +30,75 @@ const Footer = () => {
     const handleChange = event => setEmail(event.target.value)
 
     return (
-        <div className='w-full text-slate-800 dark:text-white font-normal dir-rtl'>
-            <div className='darkAnimation bg-white dark:bg-[#33353b] px-6 md:px-24'>
-                <div className='flex lg:justify-between justify-center max-lg:flex-col max-lg:flex-wrap w-full max-w-screen-xl m-auto'>
-                    <div className='flex max-lg:justify-between w-full'>
-                        <div className='ml-auto sm:px-5 my-6 lg:my-16 max-w-80'>
-                            <h6 className='text-base sm:text-lg md:text-xl font-bold'>داغترین مطالب هفته</h6>
-                            <ul className="flex flex-col mt-6 lg:mt-14 gap-4 text-sm md:text-md">
-                                {
-                                    [
-                                        { img: image1, text: 'این هفته چه بازی های معرفی می شوند.' },
-                                        { img: image2, text: 'جدیدترین تریلر بازی های جدید' },
-                                        { img: image3, text: 'این هفته چه بازی های معرفی می شوند.' }
-                                    ].map((item, idx) => (
-                                        <li key={idx} className="flex">
-                                            <Link className="flex" to="#">
-                                                <Image className="w-24 h-[4.25rem] rounded-xl" src={item.img} alt="image" />
-                                                <p className="w-40 mr-2 font-light">{item.text}</p>
-                                            </Link>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                        <div className='mx-auto px-5 min-w-fit max-sm:hidden my-6 lg:my-16'>
-                            <h6 className='text-md sm:text-lg md:text-xl font-bold'>پربازدیدترین صفحات</h6>
-                            <ul className="flex flex-col mt-6 lg:mt-14 gap-4 lg:gap-5 text-sm md:text-base">
-                                {['مقالات', 'فروشگاه', 'درباره ما', 'تماس با ما', 'جدیدترین بازی ها'].map((title, i) => (
-                                <li key={i}>
-                                    <Link to="#">{title}</Link>
-                                </li>
-                            ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className='lg:mr-10 my-6 lg:my-16 flex flex-col justify-around mx-auto w-full max-[1124px]:lg:max-w-[340px] max-w-[410px]'>
-                        <h6 className='text-xl font-bold'>عضویت در خبرنامه</h6>
-                        <div className='flex flex-col mt-6 lg:mt-14 gap-4 text-base'>
-                            <div className='flex items-center w-full '>
-                                <span className='relative w-full'>
-                                    <img className='absolute right-3 top-1' src={Message} alt='Message' />
-                                    <input className='darkAnimation bg-gray/15 dark:bg-gray outline-none w-full h-[50px] pl-2 dir-ltr placeholder:float-right pr-16 rounded-2xl placeholder:text-slate-700 dark:placeholder:text-white placeholder:font-normal' value={email} onChange={handleChange} type='email' placeholder='ایمیل خود را وارد کنید' />
-                                </span>
-                                <button onClick={savedEmail} className='bg-violet p-3 mx-2 rounded-2xl'>
-                                    <img className='h-6' src={ArrowLeft} alt='ArrowLeft' />
-                                </button>
+        <div className="w-full mt-auto z-20">
+            <div className='w-full text-slate-800 dark:text-white font-normal dir-rtl'>
+                <div className='darkAnimation bg-white dark:bg-[#33353b] px-6 md:px-24'>
+                    <div className='flex lg:justify-between justify-center max-lg:flex-col max-lg:flex-wrap w-full max-w-screen-xl m-auto'>
+                        <div className='flex max-lg:justify-between w-full'>
+                            <div className='ml-auto sm:px-5 my-6 lg:my-16 max-w-80'>
+                                <h6 className='text-base sm:text-lg md:text-xl font-bold'>داغترین مطالب هفته</h6>
+                                <ul className="flex flex-col mt-6 lg:mt-14 gap-4 text-sm md:text-md">
+                                    {
+                                        [
+                                            { img: image1, text: 'این هفته چه بازی های معرفی می شوند.' },
+                                            { img: image2, text: 'جدیدترین تریلر بازی های جدید' },
+                                            { img: image3, text: 'این هفته چه بازی های معرفی می شوند.' }
+                                        ].map((item, idx) => (
+                                            <li key={idx} className="flex">
+                                                <Link className="flex" to="#">
+                                                    <Image className="w-24 h-[4.25rem] rounded-xl" src={item.img} alt="image" />
+                                                    <p className="w-40 mr-2 font-light">{item.text}</p>
+                                                </Link>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
                             </div>
-                            <div className='bg-white py-5 w-full flex justify-between rounded-xl'>
-                                {[Enamd, Kasbokar, Samandehi].map((logo, idx) => (
-                                    <img key={idx} className="h-24 sm:h-32" src={logo} alt={`logo-${idx}`} />
-                                ))}
+                            <div className='mx-auto px-5 min-w-fit max-sm:hidden my-6 lg:my-16'>
+                                <h6 className='text-md sm:text-lg md:text-xl font-bold'>پربازدیدترین صفحات</h6>
+                                <ul className="flex flex-col mt-6 lg:mt-14 gap-4 lg:gap-5 text-sm md:text-base">
+                                    {['مقالات', 'فروشگاه', 'درباره ما', 'تماس با ما', 'جدیدترین بازی ها'].map((title, i) => (
+                                        <li key={i}>
+                                            <Link to="#">{title}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='lg:mr-10 my-6 lg:my-16 flex flex-col justify-around mx-auto w-full max-[1124px]:lg:max-w-[340px] max-w-[410px]'>
+                            <h6 className='text-xl font-bold'>عضویت در خبرنامه</h6>
+                            <div className='flex flex-col mt-6 lg:mt-14 gap-4 text-base'>
+                                <div className='flex items-center w-full '>
+                                    <span className='relative w-full'>
+                                        <img className='absolute right-3 top-1' src={Message} alt='Message' />
+                                        <input className='darkAnimation bg-gray/15 dark:bg-gray outline-none w-full h-[50px] pl-2 dir-ltr placeholder:float-right pr-16 rounded-2xl placeholder:text-slate-700 dark:placeholder:text-white placeholder:font-normal' value={email} onChange={handleChange} type='email' placeholder='ایمیل خود را وارد کنید' />
+                                    </span>
+                                    <button onClick={savedEmail} className='bg-violet p-3 mx-2 rounded-2xl'>
+                                        <img className='h-6' src={ArrowLeft} alt='ArrowLeft' />
+                                    </button>
+                                </div>
+                                <div className='bg-white py-5 w-full flex justify-between rounded-xl'>
+                                    {[Enamd, Kasbokar, Samandehi].map((logo, idx) => (
+                                        <img key={idx} className="h-24 sm:h-32" src={logo} alt={`logo-${idx}`} />
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='bg-orange h-14 px-5 md:px-24'>
-                <div className='flex text-white text-xs justify-between md:text-base items-center h-full max-w-screen-xl m-auto'>
-                    <p> &copy; کپی مطالب تنها با مجوز مکتوب مجاز است.</p>
-                    <div className='flex'>
-                        {[Twitter, Instagram, Whatsapp, Linkedin].map((icon, idx) => (
-                            <img
-                                key={idx}
-                                className={`h-5 sm:h-7 px-2 first:pr-0 last:pl-0`}
-                                src={icon}
-                                alt={`icon-${idx}`}
-                            />
-                        ))}
+                <div className='bg-orange h-14 px-5 md:px-24'>
+                    <div className='flex text-white text-xs justify-between md:text-base items-center h-full max-w-screen-xl m-auto'>
+                        <p> &copy; کپی مطالب تنها با مجوز مکتوب مجاز است.</p>
+                        <div className='flex'>
+                            {[Twitter, Instagram, Whatsapp, Linkedin].map((icon, idx) => (
+                                <img
+                                    key={idx}
+                                    className={`h-5 sm:h-7 px-2 first:pr-0 last:pl-0`}
+                                    src={icon}
+                                    alt={`icon-${idx}`}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/public/Background.js
+++ src/components/public/Background.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import Footer from '../home/Footer';
 
-const Background = ({children}) => {
+const Background = ({ children, footer=true }) => {
     return (
-        <div className="min-h-screen h-full flex flex-col bg-cover z-0 bg-center bg-no-repeat dark:bg-[url('/src/assets/background.jpg')] relative dir-rtl">
-            <div className="absolute z-10 inset-0 bg-[#f3f4f6] dark:bg-dark-gray backdrop-blur-[114px] opacity-30 dark:opacity-80 darkAnimation"></div>
-            <div className="relative z-20 flex flex-col w-full flex-grow max-w-screen px-6 md:px-24 py-5 justify-center items-center">
-                {children}
+        <>
+            <div className="min-h-screen h-full flex flex-col bg-cover z-0 bg-center bg-no-repeat dark:bg-[url('/src/assets/background.jpg')] relative dir-rtl">
+                <div className="absolute z-10 inset-0 bg-[#f3f4f6] dark:bg-dark-gray backdrop-blur-[114px] opacity-30 dark:opacity-80 darkAnimation"></div>
+                <div className="relative z-20 flex flex-col w-full flex-grow max-w-screen px-6 md:px-24 py-5  items-center">
+                    {children}
+                </div>
             </div>
-        </div>
+            {
+                footer && <Footer />
+            }
+        </>
     );
 };
 
